ba3eb402f82423312473e76fe9289bf344ded2e8
Eike Alexandre <eikealexandre555@gmail.com>
2022-11-25 16:40:51 -0300

    Node-js - v1.1

--- aula1.js
+++ aula1.js
@@ -1,8 +1,10 @@
 var http = require('http'); 
 http.createServer(function(req, res){
-
-    res.write('Brasil 3 x 1 Servia \n');
+    res.writeHead(200, {'Content-Type':'test/html'});
+    res.write('<h2>Aprendendo node.js</h2> <br>');
+    res.write(Date().substring(16,24)+'<br>');
+    res.write('Brasil 2 x 1 Suiça \n');
     res.end('Bom dia!');
 
-}).listen(3000);  // Nº da porta
+}).listen(3000);  // localhost : Nº da porta
 
