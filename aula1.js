391d65a46a1f0d834e7e85467b68856befbcfbd4
Eike Alexandre <eikealexandre555@gmail.com>
2022-11-25 16:44:28 -0300

    Node-js - v1.2 - Adicionei a biblioteca com o dia da semana

--- aula1.js
+++ aula1.js
@@ -1,10 +1,11 @@
-var http = require('http'); 
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type':'test/html'});
-    res.write('<h2>Aprendendo node.js</h2> <br>');
-    res.write(Date().substring(16,24)+'<br>');
-    res.write('Brasil 2 x 1 Suiça \n');
+var http = require('http');
+var dia = require('./biblioteca');
+http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write('<h2>Brasil 2 x 0 Servia</h2> <br>');
+    res.write('<p style=color:green>'+Date().substring(16,24)+'<br>');
+    res.write(dia.diaDaSemana()+'</p><br>')
     res.end('Bom dia!');
 
-}).listen(3000);  // localhost : Nº da porta
+}).listen(3000);
 
