ae1f4f5820a28de6e21161422df855a3f080bab0
Eike Alexandre <eikealexandre555@gmail.com>
2022-12-15 15:09:09 -0300

    Update aula3.js

--- aula3.js
+++ aula3.js
@@ -2,7 +2,7 @@
 var http = require('http');
 var url =require('url');
 
-http.createServer(function(req,res) {
+http.createServer (function(req,res) {
     res.writeHead(200, {'content-type': 'text/htm'});
     var q = url.parse(req.url, true).query;
     var txt = "<br>dia:" + q.dia + "<br>mes:" + q.mes + "<br> ano: "+q.ano;
