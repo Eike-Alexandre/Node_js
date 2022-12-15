9c45135e0ac377fa7470a2270718a708af6d6e4f
Eike Alexandre <eikealexandre555@gmail.com>
2022-12-15 15:06:34 -0300

    Update aula1.js

--- aula1.js
+++ aula1.js
@@ -1,11 +1,11 @@
-var http = require('http');
+var http = require('http'); 
 var dia = require('./biblioteca');
-http.createServer(function (req, res){
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.write('<h2>Brasil 2 x 0 Servia</h2> <br>');
-    res.write('<h3>Brasil 2 x 1 Suiça</h3> <br>');
-    res.write('<p style=color:green>'+Date().substring(16,24)+'<br>');
-    res.write(dia.diaDaSemana()+'</p><br>')
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.write('<h2>Brasil 3 x 1 Servia </h2>\n');
+    res.write('/<p style=color:green>' +Date().substring(16, 24)+ '<br>');
+    res.write( dia.diaDaSemana()+  + '</p> <br>');
     res.end('Bom dia!');
 
-}).listen(3000);
+}).listen(3000);  // Nº da porta
+
